66859ff41dd08cb3ae0054a384ece2944397ecd1
Saša Lukić <lukicsasa11@outlook.com>
2018-04-01 22:10:53 +0200

    filter

--- src/car/car-routes.js
+++ src/car/car-routes.js
@@ -4,10 +4,7 @@ const tokenAuthorize = require('../shared/tokenAuthorize');
 
 const app = express.Router();
 
-app.get('/years' ,tokenAuthorize,carController.getYears);
-app.get('/makes', tokenAuthorize,carController.getMakes);
-app.get('/models', tokenAuthorize,carController.getModels);
-app.get('/trims',tokenAuthorize, carController.getTrims);
+app.get('/filter', tokenAuthorize, carController.filter)
 app.use('/api/car', app);
 
 module.exports = app;
--- src/car/carController.js
+++ src/car/carController.js
@@ -1,55 +1,28 @@
 const BadRequestError = require('../shared/errors/BadRequestError');
 const Car = require('./car');
 
-module.exports.getYears = async (req, res, next) => {
-    const make = req.query.make;
-    const model = req.query.model;
-    const search = {};
-    if (make) search.manufacturer = make;
-    if (model) search.model = model;
-    try {
-        const years = await Car.find(search).distinct('year');
-        res.json(years);
-    } catch (err) {
-        next(err);
-    }
-}
-
-module.exports.getMakes = async (req, res, next) => {
-    const year = req.query.year;
-    const search = {};
-    if (year) search.year = year;
-    try {
-        const maunfacturers = await Car.find(search).distinct('manufacturer');
-        res.json(maunfacturers);
-    } catch (err) {
-        next(err);
-    }
-}
-
-module.exports.getModels = async (req, res, next) => {
-    const year = req.query.year;
-    const make = req.query.make;
-    const search = {
-        manufacturer: make
-    };
-    if (year) search.year = year;
-    try {
-
-        const models = await Car.find(search).distinct('model');
-        res.json(models);
-    } catch (err) {
-        next(err);
-    }
-}
-
-module.exports.getTrims = async (req, res, next) => {
+module.exports.filter = async (req, res, next) => {
     const year = req.query.year;
     const make = req.query.make;
     const model = req.query.model;
+    const result = {
+        years: [],
+        makes: [],
+        models: [],
+        trims: []
+    }
     try {
-        const trims = await Car.find({ year: year, manufacturer: make, model: model }).distinct('trim');
-        res.json(trims);
+        const search = {};
+        if (year) search.year = year;
+        if (make) search.manufacturer = make;
+        if (model) search.model = model;
+        result.years = await Car.find(search).distinct('year');
+        result.makes = await Car.find(search).distinct('manufacturer');
+        if (make)
+            result.models = await Car.find(search).distinct('model');
+        if (model)
+            result.trims = await Car.find(search).distinct('trim');
+        res.json(result);
     } catch (err) {
         next(err);
     }
